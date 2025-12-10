185cb4219ef7b7d1dcf4a58a1acdaea6e6f8af7f
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2025-12-09 23:27:56 -0300

    change config settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,21 @@
-from pydantic import BaseSettings, PostgreSQLDsn, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn
 from pathlib import Path
 
-ENV_PATH = Path(__file__).resolve().parent.parent / ".env"
+ENV_PATH = Path(__file__).resolve().parent.parent.parent / ".env"
 
 class Settings(BaseSettings):
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 180
 
-    DATABASE_URL: PostgreSQLDsn
+    DATABASE_URL: PostgresDsn
     SECRET_KEY: str
-    GOOGLE_API_KEY: str
+    GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
 
     model_config = SettingsConfigDict(
         env_file=ENV_PATH,
+        extra="ignore",
     )
 
 settings = Settings();
