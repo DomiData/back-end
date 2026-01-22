8856371fc8615f5582484745ea1638463ae597f9
Arthurabriel <arthurgb62@gmail.com>
2026-01-22 19:24:25 -0300

    feat: Include database population and ETL execution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from typing import Union
 from fastapi import FastAPI
-from app.core.database import create_db
 
+from app.core.database import create_db, SessionLocal
+from app.core.config import settings
+
+from app.etl.main_etl import run_complete_etl
+from app.utils.logger import logger
 from app import model
 async def lifespan(app: FastAPI):
     await create_db()
+    if settings.POPULATE_DB:
+        async with SessionLocal() as session:
+            logger.warning("Database populated with data!")
+            await run_complete_etl(session)
     yield
     # Shutdown code here
 
