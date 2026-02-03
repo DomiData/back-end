e36d75d2264246085f3fb469dc15ca23edd2a7b0
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2026-02-03 09:34:17 -0300

    fix: ajust in lifespan function

--- app/main.py
+++ app/main.py
@@ -1,29 +1,26 @@
-from typing import Union
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.core.database import create_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+from app.core.database import create_db, SessionLocal, get_db
 from app.core.firebase import initialize_firebase_app
 from app.core.config import settings
 from app.api.user import router as user_router
-from app.core.database import create_db, SessionLocal, get_db
-from app.core.config import settings
 from app.etl.main_etl import run_complete_etl
 from app.model.heatmap_builder import HeatmapQueryBuilderInput
 from app.services.builder import HeatMapQueryBuilder
 from app.utils.logger import logger
-from sqlalchemy.orm import Session
-
-async def lifespan(app: FastAPI):
-    await create_db()
-    if settings.POPULATE_DB:
-        async with SessionLocal() as session:
-            logger.warning("Database populated with data!")
-            await run_complete_etl(session)
-
+from app.schema.requests import NaturalSearchRequest
+from app.services.parser import QueryIntentParser, get_query_intent_parser
 
 async def lifespan(app: FastAPI):
     await create_db()
     initialize_firebase_app()
+    if settings.POPULATE_DB:
+        async with SessionLocal() as session:
+            logger.warning("Database populated with data!")
+            await run_complete_etl(session)
     yield
 
 origins = [
@@ -44,7 +41,18 @@ app.include_router(user_router)
 async def heatmap(
     params: HeatmapQueryBuilderInput,
     session: Session = Depends(get_db)
-):  
+):
+    query_builder = HeatMapQueryBuilder(session)
+    result = await query_builder.build(params)
+    return result
+
+@app.post("/heatmap/natural-search")
+async def natural_search(
+    request: NaturalSearchRequest,
+    session: AsyncSession = Depends(get_db),
+    parser: QueryIntentParser = Depends(get_query_intent_parser)
+):
+    params = parser.transform(natural_query=request.query)
     query_builder = HeatMapQueryBuilder(session)
     result = await query_builder.build(params)
     return result
