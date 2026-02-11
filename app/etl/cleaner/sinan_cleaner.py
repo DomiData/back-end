8e5b827015f24cca8973283a09d1ddd3582264a3
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2026-02-11 08:43:05 -0300

    refactor: change exceptions to catch

--- app/etl/cleaner/sinan_cleaner.py
+++ app/etl/cleaner/sinan_cleaner.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from app.utils.logger import logger 
+from app.utils.logger import logger
 
 def clean_sinan_age(age_raw):
     try:
@@ -7,9 +7,9 @@ def clean_sinan_age(age_raw):
         if age_str.startswith('4'):
             return int(age_str[1:])
         return 0
-    except:
+    except (ValueError, TypeError):
         return 0
-        
+
 
 def calcular_dv_ibge(codigo_6):
     codigo = str(codigo_6)
@@ -25,7 +25,7 @@ def calcular_dv_ibge(codigo_6):
 
     resto = soma % 10
     dv = (10 - resto) % 10
-    
+
     return f"{codigo}{dv}"
 
 
@@ -40,33 +40,33 @@ def filter_state_and_columns(df_national, state_code, mandatory_cols, optional_c
         'DT_NOTIFICACAO': 'DT_NOTIFIC'
     }
     df_national = df_national.rename(columns={k: v for k, v in mapping.items() if k in df_national.columns})
-    
+
     missing_mandatory = [c for c in mandatory_cols if c not in df_national.columns]
-    
+
     if missing_mandatory:
         logger.error(f"File rejected! Missing mandatory columns: {missing_mandatory}")
         return pd.DataFrame()
 
     existing_optional = [c for c in optional_cols if c in df_national.columns]
-    
+
     df_filtered = df_national[mandatory_cols + existing_optional].copy()
 
     for col in optional_cols:
         if col not in df_filtered.columns:
             df_filtered[col] = None
-            
+
     logger.info(f"Filtering data for State Code: {state_code} using column: ID_MUNICIP")
     df_filtered['ID_MUNICIP'] = df_filtered['ID_MUNICIP'].astype(str)
     df_state = df_filtered[df_filtered['ID_MUNICIP'].str.startswith(str(state_code))].copy()
-    
+
     if df_state.empty:
         logger.warning(f"Filtration complete, but no records found for state {state_code}.")
         return pd.DataFrame()
-    
+
     logger.debug("Formatting patient age (SINAN pattern)...")
     df_state['NU_IDADE_N'] = df_state['NU_IDADE_N'].apply(clean_sinan_age)
     df_state['ID_MUNICIP'] = df_state['ID_MUNICIP'].apply(calcular_dv_ibge)
-    
+
     logger.info(f"Successfully filtered {len(df_state)} records for the target state.")
-    
+
     return df_state
