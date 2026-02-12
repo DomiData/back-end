9a64691519e1682423f0681ea55f1f09d2fab59f
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2026-02-12 12:31:14 -0300

    remove unused imports

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Session
 
 from app.core.database import create_db, SessionLocal, get_db
 from app.core.firebase import initialize_firebase_app
